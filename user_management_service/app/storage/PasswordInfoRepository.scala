82e00c7402d3b942787ce108c8d96d8244d32df5
Anton Korzh <antonkrzh@outlook.com>
2021-03-09 16:03:31 +0200

    Allow security logic to communicate with the database

---
+++ user_management_service/app/storage/PasswordInfoRepository.scala
@@ -0,0 +1,72 @@
+package storage
+
+import com.mohiva.play.silhouette.api.LoginInfo
+import com.mohiva.play.silhouette.api.util.PasswordInfo
+import com.mohiva.play.silhouette.persistence.daos.DelegableAuthInfoDAO
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.jdbc.JdbcProfile
+import storage.db.SilhouetteTableRepository
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+import scala.reflect.ClassTag
+
+
+@Singleton
+class PasswordInfoRepository @Inject()(protected val dbConfigProvider: DatabaseConfigProvider,
+    val tableRepository: SilhouetteTableRepository
+  )(implicit ex: ExecutionContext) extends DelegableAuthInfoDAO[PasswordInfo]
+                                   with HasDatabaseConfigProvider[JdbcProfile] {
+  import profile.api._
+  import tableRepository._
+
+  override val classTag = ClassTag(classOf[PasswordInfo])
+
+  protected def searchPasswordInfo(loginInfo: LoginInfo) =
+    passwordInfoQuery.filter(_.loginInfoId in loginInfoQuery(loginInfo).map(_.id))
+
+  protected def addAction(loginInfo: LoginInfo, authInfo: PasswordInfo) = {
+    for {
+      loginInfoId <- (loginInfos returning loginInfos.map(_.id)) +=
+        DBLoginInfo(None,
+                    loginInfo.providerID,
+                    loginInfo.providerKey.toLong)
+      _ <- passwordInfoQuery +=
+        DBPasswordInfo(loginInfoId,
+                       authInfo.hasher,
+                       authInfo.password,
+                       authInfo.salt)
+    } yield ()
+  }.transactionally
+
+  protected def updateAction(loginInfo: LoginInfo, authInfo: PasswordInfo) =
+    searchPasswordInfo(loginInfo)
+      .map(dbPasswordInfo => (dbPasswordInfo.hasher, dbPasswordInfo.password, dbPasswordInfo.salt))
+      .update((authInfo.hasher, authInfo.password, authInfo.salt))
+
+  override def find(loginInfo: LoginInfo): Future[Option[PasswordInfo]] = db.run {
+    val dbPasswordInfo = for {
+      dbLoginInfo <- loginInfoQuery(loginInfo)
+      dbPasswordInfo <- passwordInfoQuery if dbPasswordInfo.loginInfoId === dbLoginInfo.id
+    } yield dbPasswordInfo
+    dbPasswordInfo.result.headOption.map(dbPIOption =>
+      dbPIOption.map(dbPI =>
+        PasswordInfo(dbPI.hasher, dbPI.password, dbPI.salt)))
+  }
+
+  override def save(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = db.run {
+    loginInfoQuery(loginInfo).joinLeft(passwordInfoQuery).on(_.id === _.loginInfoId)
+    .result.head.flatMap {
+      case (_, Some(_)) => updateAction(loginInfo, authInfo)
+      case (_, None) => addAction(loginInfo, authInfo)
+    }
+  }.map(_ => authInfo)
+
+  override def add(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] =
+    db.run(addAction(loginInfo, authInfo)).map(_ => authInfo)
+
+  override def update(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] =
+    db.run(updateAction(loginInfo, authInfo)).map(_ => authInfo)
+
+  override def remove(loginInfo: LoginInfo): Future[Unit] =
+    db.run(searchPasswordInfo(loginInfo).delete).map(_ => ())
+}
---
+++ user_management_service/conf/evolutions/default/2.sql
@@ -0,0 +1,29 @@
+# --- !Ups
+
+create table "login_info" (
+    "id" BIGSERIAL NOT NULL PRIMARY KEY,
+    "provider_id" VARCHAR(20) NOT NULL,
+    "provider_key" BIGSERIAL NOT NULL,
+
+    constraint "user_fk"
+        foreign key ("provider_key")
+        REFERENCES "users" ("id")
+        ON DELETE CASCADE
+);
+
+create table "password_info" (
+    "login_info_id" BIGSERIAL NOT NULL PRIMARY KEY,
+    "hasher" VARCHAR(20) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+    "salt" VARCHAR(100),
+
+    constraint "login_info_fk"
+        foreign key ("login_info_id")
+        REFERENCES "login_info" ("id")
+        ON DELETE CASCADE
+);
+
+# --- !Downs
+
+drop table "login_info" if exists;
+drop table "password_info" if exists;
