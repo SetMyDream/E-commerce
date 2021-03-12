2b0e4ea63a3cf8b26226ac2bbe178142de4bfaaf
Anton Korzh <antonkrzh@outlook.com>
2021-03-12 12:54:15 +0200

    Add documentation for controllers util case classes

--- user_management_service/app/controllers/validators/CredentialsValidator.scala
+++ user_management_service/app/controllers/validators/CredentialsValidator.scala
@@ -3,6 +3,11 @@ package controllers.validators
 import play.api.libs.json.Json
 
 
+/**
+ * Validates a JSON request body that consists of:
+ * @param username a string of length of 4 to 20 characters
+ * @param password a clear-text password string of length of 4 to 20 characters
+ */
 case class CredentialsValidator(username: String, password: String)
 
 object CredentialsValidator {
