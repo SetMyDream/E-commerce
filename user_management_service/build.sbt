a0363b1a8bbe3a1aec31a21b9b00055985ac5b79
Anton Korzh <antonkrzh@outlook.com>
2021-04-02 17:22:31 +0300

    Implement automatic evolutions applying for each test

--- user_management_service/build.sbt
+++ user_management_service/build.sbt
@@ -11,6 +11,7 @@ libraryDependencies ++= {
   Seq(
     guice,
     ehcache,
+    jdbc % Test,
     "net.codingwell"         %% "scala-guice"                     % "4.2.6",
     "org.typelevel"          %% "cats-core"                       % "2.2.0",
     "net.logstash.logback"    % "logstash-logback-encoder"        % "6.2",
--- user_management_service/test/util/PostgresSuite.scala
+++ user_management_service/test/util/PostgresSuite.scala
@@ -4,24 +4,37 @@ import org.scalatest._
 import com.typesafe.config.ConfigFactory
 import slick.jdbc.JdbcBackend.{Database, DatabaseDef}
 import slick.jdbc.PostgresProfile.api._
+import play.api.db.{Database => PlayDatabase, Databases}
+import play.api.db.evolutions._
 
 import scala.util.{Try, Using}
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-trait PostgresSuite extends BeforeAndAfterAll {
-  self: TestSuite =>
+trait PostgresSuite extends BeforeAndAfterAll with BeforeAndAfterEach {
+  self: Suite =>
   private val dbName = getClass.getSimpleName.toLowerCase
   private val actionTimeout = 5.seconds
 
+  private def dbUrlConfigPath = "slick.dbs.default.db.url"
   private val defaultConf = ConfigFactory.load()
-  private val dbUrlRoot = defaultConf.getString("slick.dbs.default.db.url")
+  private val dbUrlRoot = defaultConf.getString(dbUrlConfigPath)
   private val driver = defaultConf.getString("slick.dbs.default.db.profile")
   private val dbUser = defaultConf.getString("slick.dbs.default.db.user")
   private val dbPass = defaultConf.getString("slick.dbs.default.db.password")
   val dbUrl: String = "^.*/".r.findFirstIn(dbUrlRoot).get + dbName
 
-  def withDB: (DatabaseDef => ()) => Try[Unit] =
+  lazy val appDB: PlayDatabase = Databases(
+    url = dbUrlRoot,
+    driver = driver,
+    name = dbName,
+    config = Map(
+      "username" -> dbUser,
+      "password" -> dbPass
+    )
+  )
+
+  def withDB: (DatabaseDef => Unit) => Try[Unit] =
     Using(Database.forURL(dbUrl, driver = driver, user = dbUser, password = dbPass))
 
   override def beforeAll(): Unit = {
@@ -38,6 +51,15 @@ trait PostgresSuite extends BeforeAndAfterAll {
     ) { postgres =>
       Await.result(postgres.run(sqlu"DROP DATABASE #$dbName"), actionTimeout)
     }
+    appDB.shutdown()
+  }
+
+  override def beforeEach(): Unit = {
+    Evolutions.applyEvolutions(appDB)
+  }
+
+  override def afterEach(): Unit = {
+    Evolutions.cleanupEvolutions(appDB)
   }
 
 }
