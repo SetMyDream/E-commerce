fded3101e5b19abeaacb4b9b449dd475147634d6
Anton Korzh <antonkrzh@outlook.com>
2021-04-06 20:09:58 +0300

    Test isAuthenticated

--- user_management_service/test/FunctionalSpec.scala
+++ user_management_service/test/FunctionalSpec.scala
@@ -1,3 +1,6 @@
+import auth.DefaultEnv
+import auth.models.User
+import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
 import storage.UserResource
 import storage.db.UsersTableRepository
 import controllers.UserController
@@ -8,6 +11,12 @@ import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
 import play.api.test._
 import play.api.test.Helpers._
+import com.mohiva.play.silhouette.api.{Authenticator, LoginInfo, StorableAuthenticator}
+import com.mohiva.play.silhouette.api.services.AuthenticatorService
+import com.mohiva.play.silhouette.api.util.{PasswordHasherRegistry, PasswordInfo}
+import com.mohiva.play.silhouette.impl.authenticators.BearerTokenAuthenticator
+import controllers.responces.Token
+import net.codingwell.scalaguice.InjectorExtensions._
 import play.api.http.{HeaderNames, Writeable}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -25,6 +34,30 @@ class FunctionalSpec extends PlaySpec with PostgresSuite with ScalaFutures {
     }
   }
 
+  def withAuthenticatedDummyUser(
+      testCode: (DefaultEnv#I, BearerTokenAuthenticator#Value) => Any
+    ): Unit = {
+    withDummyUser { user =>
+      val userIdentity = User(user.id.get, user.username)
+      val loginInfo = LoginInfo("credentials", user.id.get.toString)
+      val passInfo = app.injector
+        .instanceOf[PasswordHasherRegistry]
+        .current
+        .hash("12345678")
+
+      val authService = app.injector
+        .instanceOf[com.google.inject.Injector]
+        .instance[AuthenticatorService[BearerTokenAuthenticator]]
+      implicit val req = FakeRequest()
+      for {
+        _ <- app.injector.instanceOf[AuthInfoRepository].add(loginInfo, passInfo)
+        authenticator <- authService.create(loginInfo)
+        token <- authService.init(authenticator)
+        _ = testCode(userIdentity, token)
+      } yield ()
+    }
+  }
+
   "UserController" should {
     "return user info by user id" in withDummyUser { user =>
       val resp = makeEmptyRequest(s"/user/${user.id}")
@@ -36,6 +69,27 @@ class FunctionalSpec extends PlaySpec with PostgresSuite with ScalaFutures {
       status(resp) mustBe 404
     }
 
+    "return user info by authentication token" in withAuthenticatedDummyUser {
+      (user, token) =>
+        val resp = makeEmptyRequest(
+          path = "/user",
+          headers = Seq(Token.httpHeaderName -> token)
+        )
+
+        status(resp) mustBe OK
+        contentAsJson(resp) mustBe Json.toJson(user)
+    }
+
+    "return 401 if invalid token is specified" in withAuthenticatedDummyUser {
+      (_, token) =>
+        val invalidToken = token.init + (token.last + 1)
+        val resp = makeEmptyRequest(
+          path = "/user",
+          headers = Seq(Token.httpHeaderName -> invalidToken)
+        )
+        status(resp) mustBe UNAUTHORIZED
+        contentAsString(resp) mustBe "User is not authenticated"
+    }
   }
 
   private def makeEmptyRequest(
