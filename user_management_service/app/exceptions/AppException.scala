aec56d8dd8d153694fef12d9daadcde1755a8954
Anton Korzh <antonkrzh@outlook.com>
2021-04-29 16:23:37 +0300

    Ensure more informative messages when the app exceptions are thrown

--- user_management_service/app/exceptions/AppException.scala
+++ user_management_service/app/exceptions/AppException.scala
@@ -1,6 +1,6 @@
 package exceptions
 
-import play.api.libs.json.{JsObject, JsValue}
+import play.api.libs.json.{JsObject, JsValue, Json}
 
 sealed trait AppException extends Throwable
 
@@ -27,30 +27,38 @@ object StorageException {
         errors: JsObject)
         extends StorageException
           with UserStorageException
-          with WalletStorageException
+          with WalletStorageException {
+    override def getMessage =
+      "Errors found in the field values:\n" + Json.prettyPrint(errors)
+  }
   final case class UnknownDatabaseError(
         msg: String = "An unknown db error! View the cause exception",
         cause: Option[Throwable])
         extends StorageException
           with UserStorageException
-          with WalletStorageException
+          with WalletStorageException {
+    override def getCause = cause.orNull
+  }
 }
 
 sealed trait VaultException extends AppException
 object VaultException {
   sealed trait TransactionalVaultException extends VaultException
   object TransactionalVaultException {
-    case object InvalidTOTP extends TransactionalVaultException {
-      val msg = "The provided TOTP code is invalid"
-    }
+    case object InvalidTOTP extends TransactionalVaultException
   }
 
   final case class UnknownVaultException(
         cause: Throwable,
         msg: String = "An unknown Vault error! View the cause exception")
         extends VaultException
-          with TransactionalVaultException
+          with TransactionalVaultException {
+    override def getCause = cause
+  }
   final case class VaultErrorResponseException(cause: JsValue)
         extends VaultException
-          with TransactionalVaultException
+          with TransactionalVaultException {
+    override def getMessage =
+      "Error response from Vault:\n" + Json.prettyPrint(cause)
+  }
 }
