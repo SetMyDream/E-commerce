faa408c77c3d2cd2f548ca1df68519b147463a60
artemv <AA2215221>
2021-05-14 18:50:11 +0300

    chatChanel as time example

--- chat_service/app/controllers/SourceDef.scala
+++ chat_service/app/controllers/SourceDef.scala
@@ -3,9 +3,16 @@ package controllers
 import play.api.libs.json.JsValue
 import akka.stream.scaladsl.Source
 
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import scala.concurrent.duration.DurationInt
+
 trait SourceDef {
   def chatChannel: Source[JsValue, _] = {
-
+    val df: DateTimeFormatter = DateTimeFormatter.ofPattern("HH mm ss")
+    val tickSource = Source.tick(0.millis, 100.millis, "TICK")
+    val s = tickSource.map(_ => df.format(ZonedDateTime.now()))
+    s
   }
 
 
