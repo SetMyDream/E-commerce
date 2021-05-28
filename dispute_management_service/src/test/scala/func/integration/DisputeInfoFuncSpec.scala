65a4585b2e6a921de13fc96d5ec7bc7c20d8c851
Anton Korzh <antonkrzh@outlook.com>
2021-05-28 20:27:05 +0300

    Implement integration tests for dispute info

--- dispute_management_service/src/test/scala/func/integration/DisputeInfoFuncSpec.scala
+++ dispute_management_service/src/test/scala/func/integration/DisputeInfoFuncSpec.scala
@@ -1,18 +1,139 @@
 package func.integration
 
+import routes.DisputeInfoRoutes
+import routes.middleware.UserAuthMiddleware
+import storage.db.repo.DisputeRepository
+import storage.model.{Dispute, DisputeStatus}
+import syntax.ioToFutureValue
 import func.share.DisputeInfoRequests
+import util.RoutesUtil
 
+import cats.effect.IO
+import cats.syntax.option._
+import org.http4s.HttpApp
+import org.http4s.Status._
+import org.http4s.circe.CirceEntityDecoder.circeEntityDecoder
+import org.http4s.implicits._
+import io.circe.generic.auto._
+import org.scalatest.EitherValues._
 import org.scalatest.wordspec.AnyWordSpec
+
+import java.time.LocalDate
 
 class DisputeInfoFuncSpec
       extends AnyWordSpec
         with IntegrationTestApp
+        with RoutesUtil
         with DisputeInfoRequests {
   import dependencies._
 
-  "this" should {
-    "work" in {
-      println(authToken)
+  "DisputeInfoRoutes" should {
+    "provide dispute info" in withDisputeAndRoutes { (dispute, routes) =>
+      val request = `request dispute info with auth token`(
+        httpConfig.authTokenHeader,
+        authToken,
+        dispute.id
+      )
+      val response = routes.run(request)
+      check(response, Ok, Some(dispute))
+    }
+
+    "return 404 if there is no such dispute" in withRepoAndRoutes { (repo, routes) =>
+      val request = `request dispute info with auth token`(
+        httpConfig.authTokenHeader,
+        authToken,
+        1
+      )
+      val response = routes.run(request)
+      check(response, NotFound, none[Dispute])
+    }
+
+    "return 401 if there is no auth token header" in withDisputeAndRoutes {
+      (dispute, routes) =>
+        val request = `request dispute info with no auth token`(dispute.id)
+        val response = routes.run(request)
+        check(response, Unauthorized, none[Dispute])
+    }
+
+    "return 401 if the auth token header is invalid" in withDisputeAndRoutes {
+      (dispute, routes) =>
+        val request = `request dispute info with auth token`(
+          httpConfig.authTokenHeader,
+          "invalidAuthToken",
+          dispute.id
+        )
+        val response = routes.run(request)
+        check(response, Unauthorized, none[Dispute])
+    }
+
+    "return 403 if a user tries to get info on some other two users' dispute" in withRepoAndRoutes {
+      (repo, routes) =>
+        val dispute = insertDispute(repo)(authedUserId + 1, authedUserId + 2, 1)
+        val request = `request dispute info with auth token`(
+          httpConfig.authTokenHeader,
+          authToken,
+          dispute.id
+        )
+        val response = routes.run(request)
+        check(response, Forbidden, none[Dispute])
+    }
+
+    "return a list of disputes related to the user" in withRepoAndRoutes {
+      (repo, routes) =>
+        val dispute1 = insertDispute(repo)(authedUserId, authedUserId + 1, 1)
+        val dispute2 = insertDispute(repo)(authedUserId, authedUserId + 2, 2)
+        val dispute3 = insertDispute(repo)(authedUserId + 1, authedUserId, 3)
+        val unrelatedDispute =
+          insertDispute(repo)(authedUserId + 1, authedUserId + 2, 4)
+        val request = `request disputes list with auth token`(
+          httpConfig.authTokenHeader,
+          authToken
+        )
+        val response = routes.run(request)
+        check(response, Ok, List(dispute1, dispute2, dispute3).some)
     }
   }
+
+  lazy val authedUserId: Long = userService.confirm(authToken).futureValue.get
+
+  def withRepoAndRoutes(
+      f: (DisputeRepository[IO], HttpApp[IO]) => Any
+    ) = withRepo { repo =>
+    val authMiddleware = new UserAuthMiddleware(httpConfig, userService)
+    val routes = DisputeInfoRoutes(authMiddleware, repo).orNotFound
+    f(repo, routes)
+  }
+
+  def withDisputeAndRoutes(
+      f: (Dispute, HttpApp[IO]) => Any
+    ) = withRepoAndRoutes { (repo, routes) =>
+    val dispute = insertDispute(repo)(authedUserId, authedUserId + 1, 1)
+    f(dispute, routes)
+  }
+
+  def insertDispute(
+      repo: DisputeRepository[IO]
+    )(buyerId: Long,
+      sellerId: Long,
+      purchaseId: Long
+    ) = {
+    val disputeId = repo.create(buyerId, sellerId, purchaseId).futureValue.value
+    makeDispute(disputeId, buyerId, sellerId, purchaseId)
+  }
+
+  def makeDispute(
+      id: Long,
+      buyerId: Long,
+      sellerId: Long,
+      purchaseId: Long
+    ) =
+    Dispute(
+      id,
+      buyerId,
+      sellerId,
+      purchaseId,
+      DisputeStatus.Active,
+      LocalDate.now()
+    )
+
 }
--- dispute_management_service/src/test/scala/func/integration/IntegrationTestApp.scala
+++ dispute_management_service/src/test/scala/func/integration/IntegrationTestApp.scala
@@ -2,21 +2,27 @@ package func.integration
 
 import config._
 import services.UserService
+import storage.db.repo.DisputeRepository
 import util.{TestingDb, TestingUserService}
 
 import cats.effect.{ContextShift, IO, Resource}
-import doobie.ExecutionContexts
 import doobie.hikari.HikariTransactor
 import org.scalatest.{Args, Status, TestSuite}
 
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
 trait IntegrationTestApp extends TestSuite {
-  implicit val CS: ContextShift[IO] = IO.contextShift(ExecutionContexts.synchronous)
-  protected val dependencies = new Dependencies(null, null, null)
+  implicit val CS: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+  protected val dependencies = new Dependencies(null, null, null, null)
+
+  protected def withRepo(f: DisputeRepository[IO] => Any) =
+    f(new DisputeRepository(dependencies.transactor))
 
   override def withFixture(test: NoArgTest) = {
     TestingDb.migrations(dependencies.transactor).use { _ =>
       IO(super.withFixture(test))
-    }.unsafeRunSync()
+    }.unsafeRunTimed(5.minutes).get
   }
 
   override def run(
@@ -25,8 +31,8 @@ trait IntegrationTestApp extends TestSuite {
     ): Status = {
     val dep = for {
       Config(_, httpConfig, dbConfig_, clientConfig_) <- configRes[IO]
-      dbConfig = dbConfig_.copy(poolSize = 1)
-      clientConfig = clientConfig_.copy(poolSize = 1)
+      dbConfig = dbConfig_.copy(poolSize = 3)
+      clientConfig = clientConfig_.copy(poolSize = 3)
       transactor <- TestingDb(dbConfig)
       userService <- TestingUserService(clientConfig, httpConfig)
       authToken <- Resource.eval(TestingUserService.acquireAuthToken(userService))
@@ -34,18 +40,21 @@ trait IntegrationTestApp extends TestSuite {
         dependencies._transactor = transactor
         dependencies._userService = userService
         dependencies._authToken = authToken
+        dependencies._httpConfig = httpConfig
       }
     } yield ()
-    dep.use(_ => IO(super.run(testName, args))).unsafeRunSync()
+    dep.use(_ => IO(super.run(testName, args))).unsafeRunTimed(5.minutes).get
   }
 
   protected class Dependencies(
         private[IntegrationTestApp] var _transactor: HikariTransactor[IO],
         private[IntegrationTestApp] var _userService: UserService[IO],
-        private[IntegrationTestApp] var _authToken: String) {
+        private[IntegrationTestApp] var _authToken: String,
+        private[IntegrationTestApp] var _httpConfig: HttpConfig) {
     def transactor = getResource(_transactor, "transactor")
     def userService = getResource(_userService, "userService")
     def authToken = getResource(_authToken, "authToken")
+    def httpConfig = getResource(_httpConfig, "httpConfig")
 
     private def getResource[R](
         res: R,
@@ -56,4 +65,5 @@ trait IntegrationTestApp extends TestSuite {
         s"Failed to initialize the dependencies: $name is missing."
       )
   }
+
 }
